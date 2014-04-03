a6235b7c510cfbcf4bf791f0faf291dcf1347d6f
“KEEVIN <keevin.mitchell@gmail.com>
2014-04-03 16:56:17 -0400

    Installing the images and hooking up the outlets

--- Soho Gourmet/Beyond2021ViewController.m
+++ Soho Gourmet/Beyond2021ViewController.m
@@ -9,6 +9,25 @@
 #import "Beyond2021ViewController.h"
 
 @interface Beyond2021ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *ibRemoveItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibAddItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibPreviousItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibNextItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibTotalOrderButton;
+@property (weak, nonatomic) IBOutlet UILabel *ibChalkboardLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *ibCurrentItemImageView;
+@property (weak, nonatomic) IBOutlet UILabel *ibCurrentItemLabel;
+
+
+
+- (IBAction)ibaRemoveItem:(id)sender;
+
+- (IBAction)ibaAddItem:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *ibaLoadPreviousItem;
+@property (weak, nonatomic) IBOutlet UIButton *ibaLoadNextItem;
+- (IBAction)ibaCalculateTotal:(id)sender;
+
 
 @end
 
@@ -26,4 +45,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)ibaRemoveItem:(id)sender {
+}
+
+- (IBAction)ibaAddItem:(id)sender {
+}
+- (IBAction)ibaCalculateTotal:(id)sender {
+}
 @end
